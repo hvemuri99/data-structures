039fa0eaf069a3fbe589e2d413b6529d97033423
Hema Sree Vemuri <hvemuri@dfcorp.drfirst.com>
2018-05-23 17:32:43 -0400

    C2: Solve two num addition using linked list.

--- data-structures-core/src/Core.java
+++ data-structures-core/src/Core.java
@@ -1,20 +1,5 @@
-import com.ds.core.alogorithms.Addition;
-
-import java.util.LinkedList;
-import java.util.List;
-
 public class Core {
 
-    public static void main(String[] args) throws Exception {
-        LinkedList<Integer> input1 = new LinkedList<>();
-        input1.addFirst(2);
-        input1.add(1, 3);
-        input1.addLast(4);
-        LinkedList<Integer> input2 = new LinkedList<>();
-        input2.add(0, 9);
-        input2.add(1, 2);
-        input2.add(2, 7);
-        Addition addTwoNum = new Addition();
-        List<Integer> output = addTwoNum.addTwoNumbersByLinkedLists(input1, input2);
+    public static void main(String[] args) {
     }
 }
